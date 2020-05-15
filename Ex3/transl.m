ee61fc237f89dfdb9746bb30ffc0536fa43dfe02
AeroPyk <aeropyk@gmail.com>
2020-05-15 19:08:50 +0200

    15/05/2020 19:07

---
+++ Ex3/transl.m
@@ -0,0 +1,22 @@
+
+%
+first enter
+manually inNode
+and outNode
+with data
+from ping_pong
+.
+c
+
+        inNodeFit = polyfit(inNode(
+:,1), inNode(:,2),1);
+outNodeFit = polyfit(outNode(
+:,1), outNode(:,2),1);
+
+outS = outNodeFit(2)
+outBwdth = 1 / outNodeFit(1)
+
+inS = inNodeFit(2)
+inBwdth = 1 / inNodeFit(1)
+
+

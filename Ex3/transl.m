ae7fa77c526bcc4d742c58341e4a69201b943a62
AeroPyk <49069309+AeroPyk@users.noreply.github.com>
2020-05-15 19:14:00 +0200

    Add files via upload

--- Ex3/transl.m
+++ Ex3/transl.m
@@ -1,17 +1,8 @@
 
-%
-first enter
-manually inNode
-and outNode
-with data
-from ping_pong
-.
-c
+%first enter manually inNode and outNode with data from ping_pong.c
 
-        inNodeFit = polyfit(inNode(
-:,1), inNode(:,2),1);
-outNodeFit = polyfit(outNode(
-:,1), outNode(:,2),1);
+inNodeFit = polyfit(inNode(:,1), inNode(:,2),1);
+outNodeFit = polyfit(outNode(:,1), outNode(:,2),1);
 
 outS = outNodeFit(2)
 outBwdth = 1 / outNodeFit(1)
